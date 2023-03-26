d8c3d5eb12432fee1c6621f0325ca4b3a0d5becf
Diana-Kaba <kabadiana327@gmail.com>
2023-03-26 12:34:47 +0300

    Added a delay and press enter

--- script.js
+++ script.js
@@ -34,29 +34,40 @@ $(function () {
   $("#ok").click(function () {
     let q = $("#question").val().trim();
     $("#question").val("");
-    $("#answers").append(`<div class="hum-answ">${q}</div>`);
-    if (q.toLowerCase().includes("привет")) {
-      $("#answers").append(`<div class="bot-answ">${hello}</div>`);
-    } else if (
-      q.toLowerCase().includes("пока") ||
-      q.toLowerCase().includes("до свидания")
-    ) {
-      $("#answers").append(`<div class="bot-answ">${bye}</div>`);
-    } else if (q.toLowerCase().includes("ты кто?")) {
-      $("#answers").append(`<div class="bot-answ">Я бот Василий :)</div>`);
-    } else if (q.toLowerCase().includes("javascript")) {
-      const jsAnsw = js[Math.floor(Math.random() * js.length)];
-      $("#answers").append(`<div class="bot-answ">${jsAnsw}</div>`);
-    } else if (q.toLowerCase().includes("html")) {
-      const htmlAnsw = html[Math.floor(Math.random() * html.length)];
-      $("#answers").append(`<div class="bot-answ">${htmlAnsw}</div>`);
-    } else if (q.toLowerCase().includes("php")) {
-      const phpAnsw = php[Math.floor(Math.random() * php.length)];
-      $("#answers").append(`<div class="bot-answ">${phpAnsw}</div>`);
-    } else {
-      const random = Math.floor(Math.random() * phrases.length);
-      const phrase = phrases[random];
-      $("#answers").append(`<div class="bot-answ">${phrase}</div>`);
+    if (q != "") {
+      $("#answers").append(`<div class="hum-answ">${q}</div>`);
+
+      setTimeout(function () {
+        if (q.toLowerCase().includes("привет")) {
+          $("#answers").append(`<div class="bot-answ">${hello}</div>`);
+        } else if (
+          q.toLowerCase().includes("пока") ||
+          q.toLowerCase().includes("до свидания")
+        ) {
+          $("#answers").append(`<div class="bot-answ">${bye}</div>`);
+        } else if (q.toLowerCase().includes("ты кто?")) {
+          $("#answers").append(`<div class="bot-answ">Я бот Василий :)</div>`);
+        } else if (q.toLowerCase().includes("javascript")) {
+          const jsAnsw = js[Math.floor(Math.random() * js.length)];
+          $("#answers").append(`<div class="bot-answ">${jsAnsw}</div>`);
+        } else if (q.toLowerCase().includes("html")) {
+          const htmlAnsw = html[Math.floor(Math.random() * html.length)];
+          $("#answers").append(`<div class="bot-answ">${htmlAnsw}</div>`);
+        } else if (q.toLowerCase().includes("php")) {
+          const phpAnsw = php[Math.floor(Math.random() * php.length)];
+          $("#answers").append(`<div class="bot-answ">${phpAnsw}</div>`);
+        } else {
+          const random = Math.floor(Math.random() * phrases.length);
+          const phrase = phrases[random];
+          $("#answers").append(`<div class="bot-answ">${phrase}</div>`);
+        }
+
+        const chatbot = document.getElementById("chatbot");
+        $("#chatbot").animate(
+          { scrollTop: chatbot.scrollHeight - chatbot.clientHeight },
+          100
+        );
+      }, 1000);
     }
     return false;
   });
@@ -64,4 +75,13 @@ $(function () {
   $("#question").click(function () {
     return false;
   });
+
+  function enterKey(event) {
+    if (event.keyCode == 13) {
+      $("#ok").click();
+      return false;
+    }
+  }
+
+  $("#question").keypress("keyup", enterKey);
 });
